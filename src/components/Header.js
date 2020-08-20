0081e67ea027616eb78f8643b9b1ffe4236fc8fe
Qi Cao <alanqicao@gmail.com>
2020-08-20 17:11:50 -0600

    header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      {/* logo on the left ->> img */}
+      {/* search box */}
+      {/*  */}
+    </div>
+  )
+}
+
+export default Header
